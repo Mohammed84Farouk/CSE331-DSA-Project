5d9995d302aec6525e9b1a673815caede23b880f
Stoorny <ahmedreda4000@yahoo.com>
2022-12-11 23:25:52 +0200

    Adding Feature to Check Opening and Closing Tag error free

--- identation.cpp
+++ identation.cpp
@@ -13,13 +13,16 @@ int main()
 
     while (getline(in,line))
     {
-        string tmp;
+        string openTag;
+        string closeTag;
         bool insidetag = false;
         bool atribute = false;
-        for (int i = 0, j = 0, k = 0; i < line.length(); i++)
+        bool tagPushed = false;
+        bool closingtag = false;
+        for (int i = 0, j = 0, k = 0 , z = 0; i < line.length(); i++)
         {
             
-            if (line[i] == ' ' && tmp.size() == 0){
+            if (line[i] == ' ' && openTag.size() == 0){
             }
             else{
                 j++;
@@ -27,23 +30,36 @@ int main()
                     insidetag = false;
                     atribute = false;
                 }
+                else if(line[i] == '<' && line[i+1] == '/') closingtag = true;
+
                 if(insidetag && line[i] == ' '){
                     atribute = true;
                 }
+
                 if(atribute) continue;
-                tmp += line[i];
+                if(!tagPushed) openTag += line[i];
+                if(closingtag) {
+                    closeTag += line[i];
+                    z++;
+                }
+
             }
             if(i < 3) continue;
 
-            if(tmp[j-1] == '<') {
+            if(openTag[j-1] == '<'){
                 k = j-1;
                 insidetag = true;
             }
 
-            if (tmp[k] == '<' && tmp[k+1] != '/' && tmp[j-1] == '>'){
-                cout << tmp << endl;
-                stack.push(tmp);
-                continue;
+            if (openTag[k] == '<' && openTag[k+1] != '/' && openTag[j-1] == '>' && !tagPushed){
+                cout << openTag << endl;
+                stack.push(openTag);
+                tagPushed = true;
+            }
+            if (closeTag[0] == '<' && closeTag[1] == '/' && closeTag[z-1] == '>'){
+                cout << closeTag << endl;
+                if(closeTag != stack.top()) cout << "ERROR "<< openTag<< "Has No Closing Tag " << endl;
+                else stack.pop();
             }
         }
     }
