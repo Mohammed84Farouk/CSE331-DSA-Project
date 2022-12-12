471bf825cc08b2886e90d4fb6c06376ba9d0d080
Stoorny <ahmedreda4000@yahoo.com>
2022-12-12 09:46:47 +0200

    Fixing indentation and code error detection

--- identation.cpp
+++ identation.cpp
@@ -11,15 +11,15 @@ int main()
     ifstream in("test.xml");
     stack<string> stack;
     ofstream out("output.xml");
+    int depth = 0;
     while (getline(in,line))
     {
         string openTag;
         string closeTag;
         string text;
-        int depth = 0;
         bool insidetag = false;
         bool atribute = false;
-        bool tagPushed = false;
+        bool tagPushed = true;
         bool closingtag = false;
         for (int i = 0, j = 0, z = 0; i < line.length(); i++)
         {
@@ -31,22 +31,26 @@ int main()
                     atribute = false;
                 }
                 else if(line[i] == '<' && line[i+1] == '/') closingtag = true;
-                else if(line[i] == '<' && line[i+1] != '/' && line[i+1] != '?' ) insidetag = true;
+                else if(line[i] == '<' && line[i+1] != '/' && line[i+1] != '?' ) {
+                    insidetag = true;
+                    tagPushed = false;
+                }
 
                 if(insidetag && line[i] == ' '){
                     atribute = true;
                 }
 
                 if(atribute) continue;
-                if(!tagPushed){
-                    openTag += line[i];
-                    j++;
-                }
+                
                 if(closingtag) {
                     closeTag += line[i];
                     z++;
                 }
-                if(!insidetag && !closingtag) text += line[i];
+                else if(!tagPushed){
+                    openTag += line[i];
+                    j++;
+                }
+                else if(!insidetag && !closingtag) text += line[i];
             }
             if(i < 3) continue;
 
@@ -55,6 +59,7 @@ int main()
                 cout<<openTag<<endl;
                 stack.push(openTag.substr(1,j-2));
                 tagPushed = true;
+                out<<endl;
                 for (int w = 0; i < depth * 4; w++)
                 {
                     out.put(' ');
@@ -64,13 +69,19 @@ int main()
                 depth++;
             }
             if (closeTag[0] == '<' && closeTag[1] == '/' && closeTag[z-1] == '>'){
-                cout << closeTag << endl;
-                /*while(closeTag.substr(2, z-3) != stack.top() && !stack.empty()){
-
+                out << text ;
+                if(closeTag.substr(2, z-3) != stack.top()){
+                    cout << '<' << '/' << stack.top() << '>' <<endl;
+                    out << '<' << '/' << stack.top() << '>' << endl;
+                    out << closeTag;
+                } 
+                else{
+                    cout << closeTag << endl;
+                    out << closeTag;
                     stack.pop();
-                } */
+                }
                 depth--;
-            }
+            }else if (i == line.length() -1) out << text;
         }
     }
 }
