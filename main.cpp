7e144263d923a955d61b66961ee0a381aca60c4c
MostafaMonged <mongedmostafa19@gmail.com>
2023-02-02 20:29:00 +0200

    xmlToJson third trial

--- main.cpp
+++ main.cpp
@@ -93,21 +93,29 @@ public:
         }
         return DataOfTags;
     }
-
 //f4
-//check if my tag is array or not, this code was a separate function but it introduces some bugs
+//check if my tag is array or not
     vector<bool> arrayTag(int tagsNamesSize, vector<string> tagsNames) {
         vector<bool> isArrayTag;
-        int j = 0;
-        for (int i = 0; i < tagsNamesSize; i++) {
+        for (int i = 0; i < tagsNamesSize-1; i++) {
+            bool Array = false;
             if (tagsNames[i].at(0) != '/') {
-                if (tagsNames[i].at(tagsNames[i].length() - 1) == 's') {
-                    isArrayTag.push_back(true);
-                } else {
-                    isArrayTag.push_back(false);
+                for (int j = i+2; j < tagsNamesSize-2 ; j++) {
+                    if(tagsNames[i+1].at(0) != '/'){ // if the next tag to the main tag (i.e tagNames[i]) is not closing tag
+                        if(tagsNames[j] == "/"+tagsNames[i+1] && tagsNames[j+1] == tagsNames[i+1]){
+                            Array= true;
+                            break;
+                        }
+                    }
+                    else{
+                        Array= false;
+                        break;
+                    }
                 }
+                isArrayTag.push_back(Array);
             }
         }
+        isArrayTag.push_back(false);
         return isArrayTag;
     }
 
@@ -229,7 +237,7 @@ public:
 int main() {
     string s = "<users><user><id>1</id><name>Ahmed Ali</name><posts><post><body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body><topics><topic> economy </topic><topic> finance </topic></topics></post><post><body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body><topics><topic> solar_energy </topic></topics></post></posts><followers><follower><id>2</id></follower><follower><id>3</id></follower></followers></user><user><id>2</id><name>Yasser Ahmed</name><posts><post><body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body><topics><topic> education </topic></topics></post></posts><followers><follower><id>1</id></follower></followers></user><user><id>3</id><name>Mohamed Sherif</name><posts><post><body> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </body><topics><topic> sports </topic></topics></post></posts><followers><follower><id>1</id></follower></followers></user></users>";
     Xml xml =  Xml(s);
-    string Json = xml.xmlToJSON();
+    string Json = xml.minifying(s);
     cout<<Json;
 }
 
